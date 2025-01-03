0e6d866000aaa29abd6c6889d833bc14ae8cc8e9
ilyag26 <gayvoronskiy.i02@gmail.com>
2025-01-03 11:18:33 +0100

    green light, red light added

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
 
+red = False
+
 models = {
     "staff" : "models/staff.obj",
     "doll" : "models/doll.obj",
@@ -14,7 +16,19 @@ textures = {
     "sky":"textures/sky.png",
     "sand":"textures/sand.png",
     "field" : "textures/field.png",
+    "hand" : "textures/hand.png"
 }
+
+class UI(Entity):
+    def __init__(self):
+        super().__init__(
+            parent=camera.ui,
+            model='sphere',
+            position=Vec2(0.5, -0.4),
+            scale=(0.3, 0.3, 0.6),
+            texture = textures['hand'],
+            rotation=(130, -50, -40)
+)
 
 class Staff: 
     def __init__(self, rot_x, rot_y, rot_z, pos_x, pos_y, pos_z, scale):
@@ -29,21 +43,26 @@ class Walls:
                         collider='box', texture = textures['field'])
         self.wall.position = Vec3(pos_x, pos_y, pos_z)
 
+text = Text(text="Green", scale=3, x=-.83, y=.45, color = color.green) 
+
+def update_text_red():
+    red = True
+    text.text = "Red"
+    text.color = color.red
+    invoke(update_text_green, delay=3)
+
+def update_text_green():
+    red = False
+    text.text = "Green"
+    text.color = color.green
+    invoke(update_text_red, delay=3)
+
+def light():
+    invoke(update_text_green, delay=3)
+
 ground = Entity(model='plane', collider='box', scale=150, texture=textures['sand'])
-
-#rot_x, rot_y, rot_z, pos_x, pos_y, pos_z, scale
-staff = Staff(0, -180, 0, 4, 0, -52, 1.5)
-staff2 = Staff(0, -180, 0, -4, 0, -52, 1.5)
-
-doll = Entity(model=models['doll'], rotation=(0, -180, 0), shader = lit_with_shadows_shader)
-doll.position = Vec3(0,0,-52)
-
-tree = Entity(model=models['tree'], rotation=(0, -90, 0), shader = lit_with_shadows_shader)
-tree.position = Vec3(0,0,-55.5)
-
-player = FirstPersonController(model='cube', z=-10, rotation=(0, -180, 0), color=color.gray, origin_y=-.5, speed=13, collider='box')
-player.collider = BoxCollider(player, Vec3(0,1,0), Vec3(1,2,1))
-player.position = Vec3(0,0,45)
+line = Entity(model='plane', scale_x = 100, scale_y = 50, color = color.red)
+line.position = Vec3(0, 0.08, -40)
 
 #scale_x, scale_z, rotation_x, rotation_y, rotation_z, pos_x, pos_y, pos_z
 front_wall = Walls(100, 70, 90, 0, 0, 0, 0, -75)
@@ -51,13 +70,27 @@ left_wall = Walls(170, 70, 90, -90, 0, 50, 0, -30)
 right_wall = Walls(170, 70, 90, 90, 0, -50, 0, -30)
 back_wall = Walls(100, 70, 90, -180, 0, 0, 0, 50)
 
-window.color = color.rgb(135,206,250)
+#rot_x, rot_y, rot_z, pos_x, pos_y, pos_z, scale
+staff = Staff(0, -180, 0, 4, 0, -52, 1.5)
+staff2 = Staff(0, -180, 0, -4, 0, -52, 1.5)
+
+doll = Entity(model=models['doll'], rotation=(0, -180, 0), shader = lit_with_shadows_shader)
+doll.position = Vec3(0, 0, -52)
+
+tree = Entity(model=models['tree'], rotation=(0, -90, 0), shader = lit_with_shadows_shader)
+tree.position = Vec3(0, 0, -55.5)
+
+player = FirstPersonController(model='cube', z=-10, rotation=(0, -180, 0), color=color.gray, origin_y=-.5, speed=13, collider='box')
+player.collider = BoxCollider(player, Vec3(0, 1, 0), Vec3(1, 2, 1))
+player.position = Vec3(0, 0, 45)
+
+window.color = color.rgb(135, 206, 250)
 
 sun = DirectionalLight()
-sun.look_at(Vec3(2,-2,-2))
+sun.look_at(Vec3(2, -2, -2))
 
 sun2 = DirectionalLight()
-sun2.look_at(Vec3(-40,40,40))
+sun2.look_at(Vec3(-40, 40, 40))
 
 pivot = Entity()
 
@@ -65,6 +98,8 @@ def input(key):
     if key == 'escape':
         quit()
 
+light()
 
+UI()
 Sky()
 app.run()
